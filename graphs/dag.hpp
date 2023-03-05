44994cbd9b1bd4ecc7e09fc1c5f4bb41280ab261
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2023-03-05 12:36:20 +0300

    intial commit

---
+++ graphs/dag.hpp
@@ -0,0 +1,31 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <alex.rom23@mail.ru> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.       Alex Romanov
+ * ----------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include "directed_graph.hpp"
+
+namespace graphs {
+
+template <typename T> struct dag : public directed_graph<graph_node<T>> {
+  using base = directed_graph<graph_node<T>>;
+  using base::contains;
+  using base::insert;
+  using typename base::value_type;
+  bool insert(const value_type &first, const value_type &second) override {
+    if (!contains(first)) insert(first);
+    if (!contains(second)) insert(second);
+    breadth_first_search search{*this};
+    auto &&cycle_possible = search(second, first);
+    if (cycle_possible) return false;
+    return base::insert_base(first, second);
+  }
+};
+
+} // namespace graphs
